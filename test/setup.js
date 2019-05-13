37d15e3735d6a10bc51f1a728af44c37bfb42041
qianyin925 <884820152@qq.com>
2019-05-13 13:12:13 +0800

    refactor: 【【其他】组件 github 库搭建】 https://www.tapd.cn/22105901/prong/stories/view/1122105901001001448

--- test/setup.js
+++
@@ -1,9 +0,0 @@
-import jsdom from 'jsdom';
-const { JSDOM } = jsdom;
-
-if (typeof document === 'undefined') {
-    const dom=new JSDOM('<!doctype html><html><head></head><body></body></html>');
-    global.window =dom.window;
-    global.document = global.window.document;
-    global.navigator = global.window.navigator;
-}
