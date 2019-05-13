2ee83e5ea8e854dc3e2a3e7af9456a333ef50e3a
dengxiaolang <302949614@qq.com>
2019-05-13 15:16:00 +0800

    refactor: 【【布局组件】面包屑】 https://www.tapd.cn/22105901/prong/stories/view/1122105901001001426

---
+++ components/breadcrumb/__test__/breadcrumb.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { assert, expect } from 'chai';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+const { mount }= Enzyme;
+import Breadcrumb from '../index.jsx';
+
+//为Enzyme 配置适配器
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Breadcrumb />', function() {
+
+  it ('判断是否能正确接收导航路由数据，及其当前路由高亮颜色', function() {
+    //渲染组件
+    const app = mount(<Breadcrumb
+      breadcrumbs={[{path: '/abcc', text: '测试1', icon: 'delete'}]}
+      color="red"/>);
+
+      //判断相不相等
+    expect(app.props().color).to.equal('red');
+    expect(app.props().breadcrumbs[0].path).to.equal('/abcc');
+    expect(app.props().breadcrumbs[0].text).to.equal('测试1');
+    expect(app.props().breadcrumbs[0].icon).to.equal('delete');
+
+    //设置props会不会改变值
+    app.setProps({ color: 'black' });
+    expect(app.props().color).to.equal('black');
+    app.setProps({ breadcrumbs: [{path: '/abcd', text: '测试2', icon: 'add'}] });
+    expect(app.props().breadcrumbs[0].path).to.equal('/abcd');
+    expect(app.props().breadcrumbs[0].text).to.equal('测试2');
+    expect(app.props().breadcrumbs[0].icon).to.equal('add');
+  });
+
+})
+
