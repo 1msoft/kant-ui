b2c45d2b042f9b9df7255d6a96f2353e80aa9c64
MeetTheBest <1278298417@qq.com>
2019-05-14 09:37:56 +0800

    refactor: 【【表单组件】数字输入框】 https://www.tapd.cn/22105901/prong/stories/view/1122105901001001451

--- components/input-number/__test__/index.test.js
+++ components/input-number/__test__/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme from 'enzyme';
 import { expect } from 'chai';
 import Adapter from 'enzyme-adapter-react-16';
-import InputNumber from '..';
+import InputNumber from '../index.jsx';
 
 const { mount } = Enzyme;
 Enzyme.configure({ adapter: new Adapter() });
@@ -21,12 +21,13 @@ describe('InputNumber', () => {
 });
 
 describe('InputNumber', () => {
-  it('能否正确出发回车事件', () => {
+  it('能否正确触发回车事件', () => {
     const wrapper = mount(<InputNumber
-      enterFunc={(value) => console.log(value)}
+      enterFunc={(value) => value}
     />);
 
     wrapper.find('input').simulate('keyup', { keyCode: 13, target: { value: 50 }});
-    expect(prop('enterFunc')).call();
+    wrapper.update();
+    expect(wrapper.find('input').prop('enterFunc')).to.equal(50);
   })
 })
--- components/input-number/index.jsx
+++ components/input-number/index.jsx
@@ -1,3 +1,8 @@
+/**
+ * 数字输入框
+ * @author jfj
+ * @module InputNumber
+ */
 import React from "react";
 import PropTypes from "prop-types";
 import { InputNumber as AntInputNumber } from "antd";
@@ -5,17 +10,18 @@ import { InputNumber as AntInputNumber } from "antd";
 /**
  * 自定义数字输入框组件
  * @param {ojbect}  props
- * @param {string}  [props.mode='box']      不同风格   'box' 'underline'
+ * @param {string}  [props.theme='box']     不同风格   'box' 'underline'
  * @param {string}  [props.label]           信息
  * @param {string}  [props.className]       附加类名
  * @param {boolean} [props.autoFocus=false] 自动获焦
  * @param {boolean} [props.controls=true]   是否显示控制器按钮
  * @param {number}  [props.precision]       数值精度
+ * @param {function}[props.enterFunc]       回车事件
  * @param {string}  [props.prefix]          格式化前缀
  * @param {string}  [props.suffix]          格式化后缀
  */
 const InputNumber = ({
-  mode,
+  theme,
   label,
   controls,
   className,
@@ -27,11 +33,11 @@ const InputNumber = ({
   const regex = new RegExp(`\\${prefix}\s?|${suffix}|(,*)`, "g");
   return (
     <AntInputNumber
-      formatter={value => `${prefix}${value}${suffix}`}
+      formatter={value => `${prefix} ${value} ${suffix}`}
       parser={value => value.replace(regex, "")}
       className={`
         em-input-number
-        em-input-number-mode-${mode}
+        em-input-number-theme-${theme}
         ${controls ? "" : "em-input-number-handler-hide"}
         ${className}
       `.replace(/\s+/, " ")}
@@ -44,7 +50,7 @@ const InputNumber = ({
 };
 
 InputNumber.propTypes = {
-  mode: PropTypes.oneOf(["box", "underline"]),
+  theme: PropTypes.oneOf(["box", "underline"]),
   label: PropTypes.string,
   className: PropTypes.string,
   autoFocus: PropTypes.bool,
@@ -56,10 +62,9 @@ InputNumber.propTypes = {
 };
 
 InputNumber.defaultProps = {
-  mode: "box",
+  theme: "box",
   autoFocus: false,
   controls: true,
-  precision: 2,
   prefix: "",
   suffix: ""
 };
