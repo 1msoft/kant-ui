700e92e77e87ada82cd1ed132a3e57e4db2722f1
System Administrator <root@kangjiexindeMacBook-Pro.local>
2019-05-14 14:58:43 +0800

    refactor: 【【布局组件】头部导航栏】 https://www.tapd.cn/22105901/prong/stories/view/1122105901001001424

--- components/header/__test__/index.test.js
+++ components/header/__test__/index.test.js
@@ -1,19 +1,36 @@
-// import React from 'react';
-// import Enzyme from 'enzyme';
-// import { expect } from 'chai';
-// import Adapter from 'enzyme-adapter-react-16';
-// import Header from '../';
+import React from 'react';
+import Enzyme from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+import Header from '../index.jsx';
 
-// const { mount } = Enzyme;
-// Enzyme.configure({ adapter: new Adapter() });
+const { mount } = Enzyme;
+Enzyme.configure({ adapter: new Adapter() });
 
-// describe('Header', () => {
-//   it('能否正确接收参数', () => {
-//     const wrapper = mount(
-//       <Header
-//         fixed={true}
-//         downHide={true}
-//         upHide={true} />
-//     );
-//   });
-// });
+describe('Header', () => {
+  it('能否正确接收参数', () => {
+    const wrapper = mount(
+      <Header
+        fixed={true}
+        downHide={true}
+        upHide={true} />
+    );
+
+    expect(wrapper.prop('fixed').to.equal(true));
+    expect(wrapper.prop('downHide').to.equal(true));
+    expect(wrapper.prop('upHide')).to.equal(true);
+  });
+});
+
+describe('Header', () => {
+  it('触发滚动事件', () => {
+    const wrapper = mount(
+      <Header
+        fixed={true}
+        downHide={200}
+        upHide={200} />
+    );
+    document.documentElement.scrollTop = 200;
+    expect(wrapper.find('.em-header-fixed').length).to.equal(1);
+  });
+});
