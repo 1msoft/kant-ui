97918ec5f7300e3e2eaa3be253045fd7dfb3291b
dengxiaolang <302949614@qq.com>
2019-05-21 16:06:32 +0800

    refactor: 【【布局组件】面包屑】 https://www.tapd.cn/22105901/prong/stories/view/1122105901001001426

--- stories/pages/index.js
+++ stories/pages/index.js
@@ -1,6 +1,6 @@
 import Welcome from './overview/Welcome';
 import Input from './form/Input';
-import Breadcrumb from './breadcrumb/Breadcrumbs';
+import Breadcrumb from './breadcrumb/Breadcrumb';
 import InputNumber from './form/InputNumber';
 
 export default [
