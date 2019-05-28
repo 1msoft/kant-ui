c53033e1c36835939f7450bb97aca64eab2d993f
dengxiaolang <302949614@qq.com>
2019-05-28 16:02:17 +0800

    refactor: 【【表单组件】文本输入框】 https://www.tapd.cn/22105901/prong/stories/view/1122105901001001433

--- components/input/index.jsx
+++ components/input/index.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import { Input as AntInput } from 'antd';
-import PropTypes from 'prop-types';
 import omit from 'omit.js';
 
 /**
  * 文本输入框
  * @param {object} props
- * @param {string} [props.className]  传入className
  */
 const Input = (props) => {
   const filterArr = [];
-  const inputProps = omit(props, filterArr);
+  const otherProps = omit(props, filterArr);
   return (
     <AntInput
-      className={props.className}
-      {...inputProps}
+      {...otherProps}
     />
   );
-};
-
-Input.propTypes = {
-  className: PropTypes.string,
 };
 
 Input.TextArea = AntInput.TextArea;
