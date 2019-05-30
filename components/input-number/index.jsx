5c2dbebc29b24ee464e464d6ec5c3b870aef6948
MeetTheBest <1278298417@qq.com>
2019-05-30 18:02:06 +0800

    refactor: 【【表单组件】数字输入框】 https://www.tapd.cn/22105901/prong/stories/view/1122105901001001451

--- components/input-number/index.jsx
+++ components/input-number/index.jsx
@@ -9,7 +9,7 @@ import PropTypes from "prop-types";
 import omit from 'omit.js';
 import {
   isEqual,
-  isEmpty,
+  isNil,
   isFunction,
 } from 'lodash';
 import classNames from 'classnames';
@@ -31,14 +31,28 @@ import { InputNumber as AntInputNumber } from "antd";
  * @see {@link https://ant.design/components/input-number-cn/#API 更多参数详见 antd 数字输入框 InputNumber 文档}
  */
 const InputNumber = (props) => {
-  const regex = new RegExp(`\\${props.prefix}\s?|${props.suffix}|(,*)`, "g");
+  const escapeCharacter = ['$', '?', '^', '*', '+'];
+  const some = (array, compare) => array.some(item => item === compare);
 
+  const transPrefix = props.prefix
+    ? some(escapeCharacter, props.prefix)
+      ? `\\${props.prefix}|`
+      : `${props.prefix}|`
+    : "";
+
+  const transSuffix = props.suffix
+    ? some(escapeCharacter, props.suffix)
+      ? `\\${props.suffix}`
+      : `${props.suffix}|`
+    : "";
+
+  const regex = new RegExp(`${transPrefix}${transSuffix}`, "g");
   const handleKeyDown = (event) => {
     const { onPressEnter, onKeyDown } = props;
     const str = String(event.target.value).replace(regex, "");
     if (
+      !isNil(onPressEnter) &&
       isFunction(onPressEnter) &&
-      !isEmpty(onPressEnter) &&
       isEqual(event.keyCode, 13)
     ) {
       onPressEnter(Number(str) || null);
