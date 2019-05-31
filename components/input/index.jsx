6f10140d1c4cf448d504ea5616ff8a0e1af0b471
dengxiaolang <302949614@qq.com>
2019-05-31 10:08:12 +0800

    fix: 【【表单组件】文本输入框】 https://www.tapd.cn/22105901/prong/stories/view/1122105901001001433

--- components/input/index.jsx
+++ components/input/index.jsx
@@ -1,25 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { Input as AntInput } from 'antd';
 import omit from 'omit.js';
+import PropTypes from 'prop-types';
 
 /**
  * 文本输入框
- * @param {object}  props
- * @param {boolean} [props.autosize=false]    文本域自适应内容高度
+ * @param {object}   props
+ * @param {boolean}  [props.autosize=false]     文本域自适应内容高度
+ * @param {function} [props.onFocus]            聚焦事件
+ * @param {function} [props.onBlur]             失焦事件
  * @see {@link https://ant.design/components/input-cn/#Input 更多参数 详见antd文本输入框 Input文档}
  */
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   const filterArr = [];
   const otherProps = omit(props, filterArr);
-  return (
-    <AntInput
-      {...otherProps}
-    />
-  );
-};
 
-const InputTextArea = (props) => {
-  const filterArr = [];
+  return (
+    <span ref={ref}>
+      <AntInput
+        {...otherProps}
+      />
+    </span>
+  );
+});
+
+const InputTextArea = React.forwardRef((props, ref) => {
+  const filterArr = [
+    'onFocus',
+    'onBlur',
+  ];
   const otherProps = omit(props, filterArr);
 
   const [isFocus, setIsFocus] = useState(false);
@@ -29,20 +38,30 @@ const InputTextArea = (props) => {
   };
 
   return (
-    <div className={setFocusClassName()}>
+    <div className={setFocusClassName()} ref={ref}>
       <AntInput.TextArea
         autosize={false}
-        onFocus={() => { setIsFocus(true);} }
-        onBlur={() => { setIsFocus(false);} }
+        onFocus={() => { setIsFocus(true); props.onFocus ? props.onFocus() : null; } }
+        onBlur={() => { setIsFocus(false); props.onBlur ? props.onBlur() : null; } }
         {...otherProps}
       />
     </div>
   );
-};
+});
 
 Input.TextArea = InputTextArea;
 Input.Search = AntInput.Search;
 Input.Group = AntInput.Group;
 Input.Password = AntInput.Password;
+
+Input.propTypes = {
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+};
+
+Input.defaultProps = {
+  onFocus: null,
+  onBlur: null,
+};
 
 export default Input;
