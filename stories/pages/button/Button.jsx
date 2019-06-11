0c8df359d684d13fc8a9e69a7c62f6ac4a800399
dengxiaolang <302949614@qq.com>
2019-06-11 18:03:35 +0800

    fix: 【【功能组件】按钮】 https://www.tapd.cn/22105901/prong/stories/view/1122105901001001468

--- stories/pages/button/Button.jsx
+++ stories/pages/button/Button.jsx
@@ -5,18 +5,20 @@ import './Button.less';
 export default () => (
   <div style={{ margin: '100px' }}>
     <div>
-      <Button
-        className='kant-button-test1'
-        type='primary'
-        onClick={() => { console.log('这个把里面的覆盖了？');} }
-      >查询</Button>
-    </div>
-    <div>
-      <Button
-        className='kant-button-test2'
-        type='primary'
-        onClick={() => { console.log('这个把里面的覆盖了？');} }
-      >这个是按钮2</Button>
+      <span>
+        <Button
+          className='kant-button-test1'
+          type='primary'
+          onClick={() => { console.log('这个把里面的覆盖了？');} }
+        >查询</Button>
+      </span>
+      <span>
+        <Button
+          className='kant-button-test2'
+          type='primary'
+          onClick={() => { console.log('这个把里面的覆盖了？');} }
+        >这个是按钮2</Button>
+      </span>
     </div>
   </div>
 );
