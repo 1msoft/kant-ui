a887bcf7ca1c6c2d18679f53cf86b46e25fc6fdd
dengxiaolang <302949614@qq.com>
2019-07-04 11:47:15 +0800

    fix: 【【KantUI】面包屑】https://www.tapd.cn/22105901/bugtrace/bugs/view?bug_id=1122105901001000622

--- stories/pages/breadcrumb/Breadcrumb.jsx
+++ stories/pages/breadcrumb/Breadcrumb.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
+import { Icon } from 'antd';
 import { Breadcrumb } from '@components/index';
 import '@components/breadcrumb/style';
 import './Breadcrumb.less';
+import '../../assets/iconfont/iconfont.css';
 
 const freeDom = (props) => {
-  return <a  href={props.path} className="kant-a">{props.text}</a>;
+  return <a  href={props.path}><Icon style={{ marginRight: '10px', fontSize: '30px' }}
+    type={'delete'}/>{props.text}</a>;
 };
 
 export default () => (
   <div>
     面包屑导航组件介绍页面 <br />
-    <div>
+    <div className={'kant-breadcrumb'}>
       <Breadcrumb
         breadcrumbProps={{
-          separator: "|"
+          separator: ">"
+        }}
+        breadcrumbItemProps={{
+          onClick: () => {
+            console.log('这个是item的默认事件');
+          }
+        }}
+        onJumpway={(url) => {
+          console.log(url, '当前跳转方法');
         }}
         targetItemClass ="kant-link"
         // itemRender={freeDom}
-        breadcrumbs={[{ path: '/abc', text: '菜单一', icon: 'edit' },
-          { path: '/abc1', text: '菜单二', icon: 'delete' }
+        breadcrumbs={[{ path: '/abc', text: '菜单一', icon: 'iconyingyongqiehuananniu' },
+          { path: '/abc1', text: '菜单二', icon: 'iconyingyongqiehuananniu' },
+          { path: '/abc1', text: '菜单三', icon: 'iconyingyongqiehuananniu' }
         ]}
       />
     </div>
